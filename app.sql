327aa7ee6be2143bab1ebd02b6d18f73d0ae01b2
ARNOLD <146892654+OMGSAM@users.noreply.github.com>
2024-11-07 14:16:38 +0100

    Create app.sql

---
+++ app.sql
@@ -0,0 +1,53 @@
+1
+SELECT COUNT(*) AS nombre_matchs 
+FROM Match 
+WHERE numJournee = 12;
+
+2
+SELECT numJournee, COUNT(*) AS Nombre 
+FROM Match 
+GROUP BY numJournee;
+
+3
+SELECT * 
+FROM Match 
+ORDER BY nombreSpectateur DESC 
+LIMIT 1;
+
+4
+SELECT 
+    SUM(
+        CASE 
+            WHEN codeEquipeLocaux = 112 AND nombreButLocaux > nombreButVisiteurs THEN 3
+            WHEN codeEquipeVisiteurs = 112 AND nombreButVisiteurs > nombreButLocaux THEN 3
+            WHEN (codeEquipeLocaux = 112 OR codeEquipeVisiteurs = 112) AND nombreButLocaux = nombreButVisiteurs THEN 1
+            ELSE 0
+        END
+    ) AS points
+FROM Match
+WHERE codeEquipeLocaux = 112 OR codeEquipeVisiteurs = 112;
+
+
+5
+CREATE PROCEDURE EquipesGagnantes(IN numJourneeParam INT)
+BEGIN
+    SELECT e.nomEquipe 
+    FROM Match m
+    JOIN Equipe e ON (m.codeEquipeLocaux = e.codeEquipe AND m.nombreButLocaux > m.nombreButVisiteurs)
+        OR (m.codeEquipeVisiteurs = e.codeEquipe AND m.nombreButVisiteurs > m.nombreButLocaux)
+    WHERE m.numJournee = numJourneeParam;
+END;
+
+
+
+
+6
+delimiter //
+CREATE TRIGGER VerifierEquipesDifferentes
+BEFORE INSERT ON Match
+FOR EACH ROW
+BEGIN
+    IF NEW.codeEquipeLocaux = NEW.codeEquipeVisiteurs THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Une équipe ne peut pas jouer contre elle-même';
+    END IF;
+END//
